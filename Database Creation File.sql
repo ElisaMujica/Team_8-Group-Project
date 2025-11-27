afde7e8f5beff5fbbe08eff22c194f7c6e8e82d3
Roland Klahitar <klahitarroland@gmail.com>
2025-11-27 09:09:01 -0600

    Implement many-to-many relationships between crop_Info, seasons, and beds

--- Database Creation File.sql
+++ Database Creation File.sql
@@ -1,120 +1,93 @@
-#Team_ Project 
-#Database Creation file 
-#Notes: If changes need to be made to design, please add comment and make change in database design: https://lucid.app/lucidchart/7184eecd-2be3-4a9c-980e-153cd7cb40c1/edit?invitationId=inv_97aa72d9-8241-4889-939a-19b138899679&page=0_0#
-#
-#
-##############################################################################################
-use group_project_team8;
-
-create table Garden (
-gardenID			Int 		NOT NULL AUTO_INCREMENT,
-gardenLocation 		Char(35) 	NOT NULL,
-CONSTRAINT			gardenPK	PRIMARY KEY(gardenID)
-);
-# Changes: removed syntax error
+CREATE DATABASE IF NOT EXISTS group_project_team8;
+USE group_project_team8;
 
 
-create table Bed (
-bedNo			Int 			NOT NULL, 
-gardenID		Int				NOT NULL, 
-size 			Int 			NULL,
-soilType		Char(10)		NULL,
-bedStatus 		Char(35)		NULL,
-CONSTRAINT		bedPK 			PRIMARY KEY(bedNo),
-CONSTRAINT		soilTypeValues 	CHECK 
-				(soilType in ('sand-loamy', 'silt-loamy', 'sandy', 'loamy','clay-loamy','clay')),
-CONSTRAINT 	 	bedStatusValues	CHECK 
-				(bedStatus IN ('Planted', 'Turned', 'Cover Crop', 'Reserved', 'Fertilized')),
-CONSTRAINT		bedAK			UNIQUE(bedUser),
-CONSTRAINT		gardenFK		FOREIGN KEY(gardenID) 
-				REFERENCES Garden(gardenID)
-                ON UPDATE CASCADE
-				ON DELETE RESTRICT
-); 
-# Changes:
-# Removed auto_increment from primary key, it is not a surrogate key and gardens have set number of beds
-# Added DELETE restriction 
-# Added ON UPDATE CASCADE
-	
-create table Crop_Info (
-cropID 			Int				NOT NULL auto_increment,
-cropName		Char(35)		not null,
-cropFamily		char(35) 		not null,
-soilType		char(25)		null,
-sunNeeds		char(25)		null,
-variety			char(25)		not null,
-seasonID		int				not null, 
-constraint		cropInfoPK		primary key(cropID),
-constraint 		cropFK			foreign key(cropName) references Current_Crop(cropName),
-constraint 		seasonFK		foreign key(seasonID) references Season(seasonID),
-constraint		cropAK			foreign key(cropFamily) references Current_Crops(cropFamily),
-constraint 		cropFamilyValues check 
-				(cropFamily in ('Alliaceae','non-rotation crop','Solanaceae','Umbelliferea','Cucurbits','Chenopodiaceae','Legumes','Brassicas'))
-);
-#Changes:
-#Added references for cropName to reference CurrentCrop
-#Addeed references for seasonID to reference Season
-
-create table Season (
-seasonID 		int				not null auto_increment,
-seasonName		char(10)		not null,
-constraint		seasonPK		primary key(seasonID),
-constraint		seasonAK		unique(seasonName),
-constraint		seasonValues	check
-				(seasonName in ('Spring','Summer','Fall','Winter','Year Round'))
+DROP TABLE IF EXISTS Crop_Info;
+CREATE TABLE Crop_Info (
+  cropID int NOT NULL AUTO_INCREMENT,
+  cropName char(35) NOT NULL,
+  cropFamily char(35) NOT NULL,
+  soilType char(25) DEFAULT NULL,
+  sunNeeds char(25) DEFAULT NULL,
+  variety char(25) NOT NULL,
+  CONSTRAINT cropInfoPK PRIMARY KEY (cropID),
+  CONSTRAINT cropFamilyValues CHECK (cropFamily in ('Alliaceae','non-rotation crop','Solanaceae','Umbelliferea','Cucurbits','Chenopodiaceae','Legumes','Brassicas'))
 );
 
-create table Current_Crops (
-cropName 		char(35)		not null,
-plantStatus 	char(25) 		null,
-yearPlanted		year			not null,
-constraint 		currentCropPK	primary key(cropName),
-constraint		currentCropFK	foreign key(cropFamily) references Crop_Info(cropFamily),
-constraint		bedFK			foreign key(bedNo) references Bed(BedNo),
-constraint 		cropIdFK		foreign key(cropID) references Crop_Info(cropID),
-constraint		seasonID 		foreign key(seasonName) references Season(seasonName)
-#concat seasonName and yearplanted to get semesterPlanted
-);
-#Added references
+--
+-- Table structure for table Season
+--
 
-create table Reserver (
-firstName		char(35)		not null,
-lastName		char(35) 		not null,
-email 			char(50) 		null,
-semesterAssigned char(35) 		not null,
-constraint		reserverPK		primary key(lastName),
-constraint 		reserverNameAK	unique(firstName),
-constraint 		reserverEmailAK	unique(email),
-constraint		reserverFK		foreign key(bedNo) references Bed(bedNo)
-);
-#Added references to reserverFK
+DROP TABLE IF EXISTS Season;
 
-create table Inventory (
-productName		char(50) 		not null,
-quantityInGarden int 			null,
-constraint		inventoryPK		primary key(productName),
-constraint		inventoryFK 	foreign key(productID) references InventoryTransactions(productID)
+CREATE TABLE Season (
+  seasonID int NOT NULL AUTO_INCREMENT,
+  seasonName char(10) NOT NULL,
+  CONSTRAINT seasonPK PRIMARY KEY (seasonID),
+  UNIQUE KEY seasonAK (seasonName),
+  CONSTRAINT seasonValues CHECK (seasonName in ('Spring','Summer','Fall','Winter','Year Round'))
 );
-#Changes foreign key name from productID to -> inventoryFK
-#Added reference to inventoryFK
 
-create table InventoryTransactions (
-productID		int				not null auto_increment,
-quantityPurchased int			not null,
-datePurchased	date			not null,
-price			decimal(10,2)	not null,
-constraint		InventoryTransPK primary key(productID),
-constraint 		InventoryTransAK unique(datePurchased),
-constraint		InventoryTransFK foreign key(productName) references Inventory(productName)
-);
-#Added reference to InventoryTransFK
 
-create table FoodWaste (
-dateReported	date			not null,
-wasteType		char(20)		not null,
-weight			int				not null,
-constraint		FoodWastePK		primary key(dateReported),
-constraint 		FoodWasteValue	check
-				(wasteType in ('Produce','Coffee Grounds'))
+-- Table structure for table Crop_Season
+--
+
+DROP TABLE IF EXISTS Crop_Season;
+CREATE TABLE Crop_Season (
+  cropID int NOT NULL,
+  seasonID int NOT NULL,
+  CONSTRAINT Crop_SeasonPK PRIMARY KEY (cropID,seasonID),
+  CONSTRAINT Crop_InfoFK FOREIGN KEY (cropID) REFERENCES Crop_Info (cropID),
+  CONSTRAINT SeasonFK FOREIGN KEY (seasonID) REFERENCES Season (seasonID)
 );
-#Removed unique constraint on waste type 
+
+-- Table structure for table Current_Crops
+--
+
+DROP TABLE IF EXISTS Current_Crops;
+
+CREATE TABLE Current_Crops (
+  cropName char(35) NOT NULL,
+  plantStatus char(25) DEFAULT NULL,
+  cropID int NOT NULL,
+  CONSTRAINT currentCropPK PRIMARY KEY (cropName),
+  CONSTRAINT cropIdFK FOREIGN KEY (cropID) REFERENCES Crop_Info (cropID)
+);
+
+
+Create TABLE Garden (
+    gardenID INT NOT NULL AUTO_INCREMENT,
+    gardenName char(30) NOT NULL,
+    gardenLocation Char(30),
+    CONSTRAINT gardenPK PRIMARY KEY (gardenID)
+);
+
+CREATE TABLE Bed (
+    bedNO char(5) NOT NULL,
+    size decimal(10, 2) NOT NULL,
+    soilType char(10) NOT NULL,
+    bedStatus char(35) NOT NULL,
+    gardenID INT NOT NULL,
+    CONSTRAINT bedPK PRIMARY KEY (bedNO),
+    CONSTRAINT bedSize CHECK (size > 0),
+    CONSTRAINT bedSoilType CHECK (soilType IN ('sand-loamy', 'silt-loamy', 'sandy', 'loamy','clay-loamy','clay')),
+    CONSTRAINT bedStatus CHECK (bedStatus IN ('Planted', 'Turned', 'Cover Crop', 'Reserved', 'Fertilized') ),
+    CONSTRAINT gardenFK FOREIGN KEY (gardenID) REFERENCES Garden (gardenID)
+);
+
+
+CREATE TABLE Growing_Bed(
+  cropName char(35) NOT NULL,
+  bedNO char(5) NOT NULL,
+  datePlanted DATE NOT NULL,
+  dateHarvested DATE NULL,
+  issuesEncountered TEXT NULL,
+  CONSTRAINT Growing_BedPK PRIMARY KEY (cropName, bedNO, datePlanted),
+  CONSTRAINT Current_CropsFK FOREIGN KEY (cropName) REFERENCES Current_Crops(cropName)
+        ON UPDATE CASCADE ON DELETE RESTRICT,
+  CONSTRAINT BedFK FOREIGN KEY (bedNO) REFERENCES Bed(bedNO)
+        ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT ValidHarvestDate CHECK (dateHarvested IS NULL OR dateHarvested >= datePlanted)
+);
+
+

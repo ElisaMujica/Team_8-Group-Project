2d1e0f1154dd04a11dd4ad40f9b98024d48461a2
Roland Klahitar <32585680+rolank@users.noreply.github.com>
2025-11-29 17:34:20 -0600

    Insert initial bed data into Bed table

---
+++ bed_polulation.sql
@@ -0,0 +1,7 @@
+INSERT INTO Bed (bedNO, size, soilType, bedStatus, gardenID)
+VALUES
+('B7', 10.00, 'sand-loamy', 'Planted', 1),
+('B3', 10.00, 'silt-loamy', 'Turned', 1),
+('B2', 10.00, 'silt-loamy', 'Turned', 1),
+('B2S', 10.00, 'loamy', 'Planted', 2),
+('B3S', 10.00, 'loamy', 'Planted', 2);

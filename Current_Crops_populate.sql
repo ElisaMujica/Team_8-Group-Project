e39060176dbd46367ffaef2ee4aa118d74f07513
Roland Klahitar <32585680+rolank@users.noreply.github.com>
2025-11-29 18:03:15 -0600

    Current_Crops populate script. In case that a Crop doesn't match a season, please update it.
    Populated the Current_Crops table with various crops and their statuses.

---
+++ Current_Crops_populate.sql
@@ -0,0 +1,23 @@
+
+USE group_project_team8;
+
+INSERT INTO Current_Crops (cropSeasonID, cropName, plantStatus, cropID, semesterPlanted, yearPlanted) VALUES
+(1, 'mexican mint', 'repotted', 1, 'Spring', 2023),
+(2, 'okra', 'planted', 2, 'Summer', 2024),
+(3, 'onion', 'repotted', 3, 'Fall', 2025),
+(4, 'aloe vera', 'planted', 4, 'Spring', 2022),
+(5, 'tomatoes', 'repotted', 5, 'Summer', 2023),
+(6, 'cumin', 'planted', 6, 'Fall', 2024),
+(7, 'melon', 'repotted', 7, 'Spring', 2025),
+(8, 'beets', 'planted', 8, 'Summer', 2022),
+(9, 'green beans', 'repotted', 9, 'Fall', 2023),
+(10, 'brocolli', 'planted', 10, 'Spring', 2024),
+(11, 'squash', 'repotted', 11, 'Summer', 2025),
+(12, 'sweet potato', 'planted', 12, 'Fall', 2022),
+(13, 'pak choi', 'repotted', 13, 'Spring', 2023),
+(14, 'loofah', 'planted', 14, 'Summer', 2024),
+(15, 'cherry tomatoes', 'repotted', 15, 'Fall', 2025),
+(16, 'peppers', 'planted', 16, 'Spring', 2022),
+(17, 'cauliflower', 'repotted', 17, 'Summer', 2023),
+(18, 'eggplant', 'planted', 18, 'Fall', 2024),
+(19, 'kale', 'repotted', 19, 'Spring', 2025);

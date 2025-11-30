7d7a7fcd18c358b10bf05aa1a1ab136f6ce8a68e
Roland Klahitar <32585680+rolank@users.noreply.github.com>
2025-11-29 20:21:04 -0600

    Add new reservation entries for users

--- ReservationPopulation.sql
+++ ReservationPopulation.sql
@@ -1,6 +1,9 @@
 ## Reservation population
-INSERT INTO Reservation(userID, bedNO, semesterAssigned)  VALUES(1, 'B2', '2025-11-29');
-INSERT INTO Reservation(userID, bedNO, semesterAssigned)  VALUES(1, 'B2S', '2025-01-20');
-INSERT INTO Reservation(userID, bedNO, semesterAssigned)  VALUES(2, 'B3', '2025-07-09');
-INSERT INTO Reservation(userID, bedNO, semesterAssigned)  VALUES(3, 'B3S', '2024-12-31');
-INSERT INTO Reservation(userID, bedNO, semesterAssigned)  VALUES(4, 'B7', '2025-06-29'); 
+INSERT INTO Reservation(userID, bedNO, semesterAssigned)  VALUES(1, 'CSTB1', '2025-11-29');
+INSERT INTO Reservation(userID, bedNO, semesterAssigned)  VALUES(1, 'CSTB2', '2025-01-20');
+INSERT INTO Reservation(userID, bedNO, semesterAssigned)  VALUES(2, 'CSTB3', '2025-07-09');
+INSERT INTO Reservation(userID, bedNO, semesterAssigned)  VALUES(2, 'CSTB7', '2025-07-09');
+INSERT INTO Reservation(userID, bedNO, semesterAssigned)  VALUES(3, 'CSTB4', '2024-12-31');
+INSERT INTO Reservation(userID, bedNO, semesterAssigned)  VALUES(3, 'SUSB1', '2024-12-31');
+INSERT INTO Reservation(userID, bedNO, semesterAssigned)  VALUES(4, 'CSTB5', '2025-06-29'); 
+INSERT INTO Reservation(userID, bedNO, semesterAssigned)  VALUES(4, 'SUSB5', '2025-06-29'); 

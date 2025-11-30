4c0946116df23b2fd27293192cc27d307e8e7311
Roland Klahitar <32585680+rolank@users.noreply.github.com>
2025-11-29 18:48:47 -0600

    Reservation population

---
+++ ReservationPopulation.sql
@@ -0,0 +1,6 @@
+## Reservation population
+INSERT INTO Reservation(userID, bedNO, semesterAssigned)  VALUES(1, 'B2', '2025-11-29');
+INSERT INTO Reservation(userID, bedNO, semesterAssigned)  VALUES(1, 'B2S', '2025-01-20');
+INSERT INTO Reservation(userID, bedNO, semesterAssigned)  VALUES(2, 'B3', '2025-07-09');
+INSERT INTO Reservation(userID, bedNO, semesterAssigned)  VALUES(3, 'B3S', '2024-12-31');
+INSERT INTO Reservation(userID, bedNO, semesterAssigned)  VALUES(4, 'B7', '2025-06-29'); 

d86effd94182fa764a03d9c524619b592fb001c1
Roland Klahitar <32585680+rolank@users.noreply.github.com>
2025-11-29 19:03:04 -0600

    item population

---
+++ itemPopulation.sql
@@ -0,0 +1,10 @@
+## item population
+
+INSERT INTO item (itemID, itemName, itemType) VALUES
+(1, '9ft 2x4s', 'Lumber'),
+(2, '9ft Zinc Sheets', 'Metal'),
+(3, 'Sliding Latches', 'Hardware'),
+(4, '6in Heavy Duty Hinges', 'Hardware'),
+(5, 'Chicken Wire', 'Fencing'),
+(6, '9ft Plastic Sheet', 'Plastic'),
+(7, 'Crane Scales', 'Tool');
